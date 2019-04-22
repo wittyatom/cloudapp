364adc790d90b1554d9a496d1caa90c5daa9f902
Harshit <harshitmanchanda75@gmail.com>
2019-04-21 21:46:21 -0400

    first message

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
-from flask import Flask
+#from flask import Flask
+from flask import Flask, render_template, json, request, session, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    return render_template('index.html')
+
+@app.route("/main")
+def return_main():
+    return render_template('index.html')
